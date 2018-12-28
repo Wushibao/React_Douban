41db7f81f706657b68133c1a64feadcb556ec9fd
j4wu <jim.b.wu@nokia.com>
2018-12-28 19:14:57 +0800

    configuration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Button>START</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
